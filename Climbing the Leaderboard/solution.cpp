920d6a33f88f11c27119837c96cf9e85e5b9b23c
Naman00 <42418828+Naman00@users.noreply.github.com>
2020-05-13 17:25:18 +0530

    Create solution.cpp

---
+++ Climbing the Leaderboard/solution.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    int m;
+    cin>>m;
+    int b[m];
+    for(int i=0;i<m;i++)
+    {
+        cin>>b[i];
+    }
+    int r[n]={0};
+    // int rank=0;
+    r[0]=1;
+    for(int i=0;i<n-1;i++)
+    {
+        if(a[i+1]==a[i])
+            r[i+1]=r[i];
+        else if(a[i+1]<a[i])
+            r[i+1]=r[i]+1;
+
+    }
+    int k=n-1;
+    for(int i=0;i<m;i++)
+    {
+       for(int j=k;j>=0;j--)
+       {
+           if(b[i]<a[j])
+           {
+            cout<<r[j]+1<<endl;
+            k=j;
+            break;
+
+           }
+            else if(b[i]==a[j])
+            {
+                cout<<r[j]<<endl;
+                 k=j;
+                break;
+            }
+            else if(b[i]>a[j] && j==0)
+            {
+                cout<<r[j]<<endl;
+                // cout<<1;
+                break;
+            }
+       } 
+       
+    }
+    // cout<<1;
+}
